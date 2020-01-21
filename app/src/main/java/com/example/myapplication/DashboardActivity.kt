78aa8f5ab1a1d4a8dd62c67df80a05203c4668f7
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-01-21 14:43:31 +0530

    updated the ui

--- app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -3,10 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddTaskActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        setContentView(R.layout.activity_dashboard)
     }
 }
